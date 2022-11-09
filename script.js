126fb25d01bb9e858d42389f4adfb1fda837eae0
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2022-11-10 00:24:24 +0100

    Räkna ut totala priset på enskilda munksorter.

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const buttonAdd = document.querySelector("#add");
 const buttonRemove = document.querySelector("#remove");
 let donutAmount = document.querySelector("#donutCurrentAmount");
 let donutPrice = document.querySelector("#donutCurrentPrice");
+let totalPrice = document.querySelector("#totalPriceAll")
+
+const strawberryPrice = 15;
 
 //add donut
 buttonAdd.addEventListener('click', function() {
     donutAmount.value = Number(donutAmount.value) + 1;
+    donutPrice.value = Number(donutPrice.value) + strawberryPrice;
 });
 
 //remove donut
 buttonRemove.addEventListener('click', function() {
     if(donutAmount.value > 0) {
         donutAmount.value = Number(donutAmount.value) - 1;
+        donutPrice.value = Number(donutPrice.value) - strawberryPrice;
     }
 });
