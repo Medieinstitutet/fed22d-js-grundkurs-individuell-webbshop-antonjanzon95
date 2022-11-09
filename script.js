a327a697603e7c705612741571612c7b41751db0
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2022-11-10 00:51:02 +0100

    Totalt alla munkar + HTML-select.

--- script.js
+++ script.js
@@ -6,16 +6,22 @@ let totalPrice = document.querySelector("#totalPriceAll")
 
 const strawberryPrice = 15;
 
-//add donut
+totalPrice.textContent = 'Totalt: 0kr';
+
+//add donut & calculate total
 buttonAdd.addEventListener('click', function() {
     donutAmount.value = Number(donutAmount.value) + 1;
+    //Detta bör appendas till en array där man kan räkna ut totalen för alla munkar ifrån?
     donutPrice.value = Number(donutPrice.value) + strawberryPrice;
+    totalPrice.textContent = `Totalt: ${donutPrice.value}kr`;
 });
 
-//remove donut
+//remove donut & calculate total
 buttonRemove.addEventListener('click', function() {
-    if(donutAmount.value > 0) {
+    if (donutAmount.value > 0) {
         donutAmount.value = Number(donutAmount.value) - 1;
+        //Detta bör appendas till en array där man kan räkna ut totalen för alla munkar ifrån?
         donutPrice.value = Number(donutPrice.value) - strawberryPrice;
+        totalPrice.textContent = `Totalt: ${donutPrice.value}kr`;
     }
 });
