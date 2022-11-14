cd6da577ac9810ffe2c4ecd5a6cc61f4e1eef491
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2022-11-14 14:46:13 +0100

    ESLint, Prettier, clear form knapp.

--- script.js
+++ script.js
@@ -1,61 +1,72 @@
-const buttonAdd = document.querySelectorAll(".add");
-const buttonRemove = document.querySelectorAll(".remove");
+/* eslint-disable no-plusplus */
+const buttonAdd = document.querySelectorAll('.add');
+const buttonRemove = document.querySelectorAll('.remove');
+const buttonClear = document.querySelector('#clearForm');
 
+// calculate sum of current donut
+function updateSumCurrent(e) {
+  const donutPrice = e.querySelector('.donutPrice').textContent;
+  const donutAmount = e.querySelector('.donutAmount').textContent;
 
-//increase donut amount by 1
+  const sum = donutAmount * donutPrice;
+
+  e.querySelector('.donutSumPrice').textContent = sum;
+}
+
+// calculate sum of all donuts
+function updateSumAll() {
+  const donutRows = document.querySelectorAll('.row');
+  let total = 0;
+
+  for (let i = 0; i < donutRows.length; i++) {
+    const currentDonutTotal = Number(
+      donutRows[i].querySelector('.donutSumPrice').textContent,
+    );
+
+    total += currentDonutTotal;
+  }
+  document.querySelector('#sumPriceAll').textContent = `Totalt: ${total} kr.`;
+}
+
+// increase donut amount by 1
 function increaseDonutAmount(e) {
-    const amountEl = e.currentTarget.parentElement.querySelector(".donutAmount");
+  const amountEl = e.currentTarget.parentElement.querySelector('.donutAmount');
 
-    let amount = Number(amountEl.textContent);
+  const amount = Number(amountEl.textContent);
 
-    amountEl.textContent = amount + 1;
+  amountEl.textContent = amount + 1;
 
-    updateSumCurrent(e.currentTarget.parentElement);
-    updateSumAll(e.currentTarget.parentElement);
+  updateSumCurrent(e.currentTarget.parentElement);
+  updateSumAll(e.currentTarget.parentElement);
 }
 
-//decrease donut amount by 1
+// decrease donut amount by 1
 function decreaseDonutAmount(e) {
-    const amountEl = e.currentTarget.parentElement.querySelector(".donutAmount");
+  const amountEl = e.currentTarget.parentElement.querySelector('.donutAmount');
 
-    let amount = Number(amountEl.textContent);
+  const amount = Number(amountEl.textContent);
 
-    if(amount < 1) {
-        return;
-    }
+  if (amount < 1) {
+    return;
+  }
 
-    amountEl.textContent = amount - 1;
+  amountEl.textContent = amount - 1;
 
-    updateSumCurrent(e.currentTarget.parentElement);
-    updateSumAll(e.currentTarget.parentElement);
+  updateSumCurrent(e.currentTarget.parentElement);
+  updateSumAll(e.currentTarget.parentElement);
 }
 
-//calculate sum of current donut
-function updateSumCurrent (e) {
-    const donutPrice = e.querySelector(".donutPrice").textContent;
-    const donutAmount = e.querySelector(".donutAmount").textContent;
-
-    const sum = donutAmount * donutPrice;
-
-    e.querySelector(".donutSumPrice").textContent = sum;
-}
-
-//calculate sum of all donuts
-function updateSumAll () {
-    const donutRows = document.querySelectorAll(".row");
-    let total = 0;
-
-    for (let i = 0; i < donutRows.length; i++) {
-        const currentDonutTotal = Number(donutRows[i].querySelector(".donutSumPrice").textContent);
-
-        total = total + currentDonutTotal;
-    }
-    document.querySelector("#sumPriceAll").textContent = "Totalt: " + total + "kr."
-}
-
-//add click functions to buttons +/-
+// add click functions to buttons +/-
 for (let i = 0; i < buttonAdd.length; i++) {
-    buttonAdd[i].addEventListener('click', increaseDonutAmount);
-    buttonRemove[i].addEventListener('click', decreaseDonutAmount);
+  buttonAdd[i].addEventListener('click', increaseDonutAmount);
+  buttonRemove[i].addEventListener('click', decreaseDonutAmount);
 }
 
+// clear checkout form
+function clearForm() {
+  const checkoutForm = document.querySelector('#checkoutForm');
+  checkoutForm.reset();
+}
+
+// add click function to clear form button
+buttonClear.addEventListener('click', clearForm);
